8930d5e56998cf2c0c7d0b63bd66005417f6e882
ginigit <839256767@qq.com>
2019-12-18 21:22:12 +0800

    反转链表

---
+++ 001_reverse_list.cpp
@@ -0,0 +1,33 @@
+
+/*
+ * 001.反转链表
+ * 问题描述: 输入一个链表，反转链表后，输出新链表的表头。
+ * 提示: three pointers
+ */
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+        if (pHead == NULL) { return NULL; }
+        
+        ListNode* p = pHead;
+        ListNode* q = p->next;
+        while (q != NULL)
+        {
+            ListNode* k = q->next;
+            q->next = p;
+            
+            p = q;
+            q = k;
+        }
+        
+        pHead->next = NULL;
+        return p;
+    }
+};
